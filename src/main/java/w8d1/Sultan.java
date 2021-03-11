3e5390c32e67fa096b1635e640079e7ae5191270
n0rb1v <n0rb1v@gmail.com>
2021-03-11 15:30:53 +0100

    1

--- src/main/java/w8d1/Sultan.java
+++ src/main/java/w8d1/Sultan.java
@@ -1,7 +1,28 @@
 package w8d1;
 
 public class Sultan {
-    public int openDoors() {
-        return 0;
+    public boolean[] openDoors() {
+        boolean[] cells = new boolean[100];
+        for (int day = 1; day < 101; day++) {
+            for (int i = 1; i < 101; i += day) {
+                cells[i - 1] = !cells[i - 1];
+            }
+        }
+        return cells;
+    }
+
+    public String print(boolean[] b) {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < 100; i++) {
+            if (b[i] == true) {
+                result.append(i+"\n");
+            }
+        }
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        Sultan s = new Sultan();
+        System.out.println(s.print(s.openDoors()));
     }
 }
